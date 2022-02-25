e55ae52190fbb3fa7631e3c7317aba04957aaa16
luipe <luipe@DESKTOP-0134ARJ>
2022-02-25 18:26:45 +0100

    Commit 3

--- src/cuentas/CCuenta.java
+++ src/cuentas/CCuenta.java
@@ -1,40 +1,68 @@
 package cuentas;
-
+/**
+ * 
+ * @author Pedro Luis
+ *
+ */
 public class CCuenta {
 	
 	private String nombre;
     private String cuenta;
     private double saldo;
     private double tipoInterés;
-
+/**
+ * 
+ * @return
+ */
     private String getNombre() {
 		return nombre;
 	}
-
+/**
+ * 
+ * @param nombre
+ */
 	private void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-
+/**
+ * 
+ * @return
+ */
 	private String getCuenta() {
 		return cuenta;
 	}
-
+	/**
+	 * 
+	 * @param cuenta
+	 */
 	private void setCuenta(String cuenta) {
 		this.cuenta = cuenta;
 	}
-
+	/**
+	 * 
+	 * @return
+	 */
 	private double getSaldo() {
 		return saldo;
 	}
-
+	/**
+	 * 
+	 * @param saldo
+	 */
 	private void setSaldo(double saldo) {
 		this.saldo = saldo;
 	}
-
+	/**
+	 * 
+	 * @return
+	 */
 	private double getTipoInterés() {
 		return tipoInterés;
 	}
-
+	/**
+	 * 
+	 * @param tipoInterés
+	 */
 	private void setTipoInterés(double tipoInterés) {
 		this.tipoInterés = tipoInterés;
 	}
@@ -42,26 +70,43 @@ public class CCuenta {
 	public CCuenta()
     {
     }
-
+	/**
+	 * 
+	 * @param nom
+	 * @param cue
+	 * @param sal
+	 * @param tipo
+	 */
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         setNombre(nom);
         setCuenta(cue);
         setSaldo(sal);
     }
-
+    /**
+     * 
+     * @return
+     */
     public double estado()
     {
         return getSaldo();
     }
-
+    /**
+     * 
+     * @param cantidad
+     * @throws Exception
+     */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
             throw new Exception("No se puede ingresar una cantidad negativa");
         setSaldo(getSaldo() + cantidad);
     }
-
+    /**
+     * 
+     * @param cantidad
+     * @throws Exception
+     */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
--- src/cuentas/Main.java
+++ src/cuentas/Main.java
@@ -6,17 +6,23 @@ package cuentas;
  *
  */
 public class Main {
+	/**
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
         operativa_cuenta(null);
     }
-
+/**
+ * 
+ * @param cantidad
+ */
 	private static void operativa_cuenta(float cantidad) {
 		CCuenta cuenta1;
 		double saldoActual;
 		cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
         saldoActual = cuenta1.estado();
         System.out.println("El saldo actual es"+ saldoActual );
-
         try {
             cuenta1.retirar(2300);
         } catch (Exception e) {
